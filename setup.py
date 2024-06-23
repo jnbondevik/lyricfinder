b8be31c9d8817adeea1759ff47ae4c321b3a040e
Jon Nicolas <jonnico96@icloud.com>
2024-06-23 17:59:04 +0200

    Add dependencies.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="lyricfinder",
     version="1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['selenium'],
     entry_points={
       'console_scripts': ['lyricfinder=source.main:main']
     }
