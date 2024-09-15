5609cc2b4addd9ae517dd8bde351e9f48f492cb0
Jon Nicolas <jonnico96@icloud.com>
2024-09-15 12:31:14 +0200

    Replace chrome-driver with requests.

--- source/main.py
+++ source/main.py
@@ -1,54 +1,47 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import argparse
-
-def format_arguments(artist: str, song: str):
-  return artist.lower(), song.replace(" ", "").lower()
+from bs4 import BeautifulSoup
+from os import get_terminal_size
+import re
+import requests
 
 def format_lyrics(lyrics: str):
-  # remove comment
-  lyrics = "".join(lyrics.split("-->")[1:])
-  # remove <br>
-  lyrics = lyrics.replace(r"<br>", "")
-  return lyrics
+  ''' Remove tags and whitespace. '''
+  lyrics = re.findall(r'(^[^<]+)', lyrics)
+  lyrics = ''.join([line for line in lyrics]).strip()
+  return termcenter(lyrics)
 
 def get_lyrics(artist, song):
-  op = webdriver.ChromeOptions()
-  op.add_argument("headless")
-  driver = webdriver.Chrome(options=op)
-  driver.get(f"https://www.azlyrics.com/lyrics/{artist}/{song}.html")
+  ''' Scrapes AZLyrics to get lyrics. '''
+  artist, song = artist.replace(' ', ''), song.replace(' ', '')
+  url = f'https://www.azlyrics.com/lyrics/{artist}/{song}.html'
   try:
-    # find parent node of comment before the song lyrics
-    lyrics = driver.find_element(By.XPATH, "//comment()[contains(., 'azlyrics.com content by')]/..")
-    # get the HTML
-    lyrics = lyrics.get_attribute("innerHTML")
-    lyrics = format_lyrics(lyrics)
-  except Exception:
-    print("Lyrics not found! Check name.")
-    lyrics = None
-  finally:
-    driver.close()
-  return lyrics
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # locate lyrics
+    textsoup = soup.find('div', class_='col-xs-12 col-lg-8 text-center')
+    lyrics = textsoup.find('div', class_=None).text
+    return format_lyrics(lyrics)
+  except:
+    raise EOFError('Song not found.')
+
+def termcenter(text):
+  ''' Centers text-output. '''
+  width = get_terminal_size(0).columns
+  midtext = ''
+  for line in text.split('\n'):
+    remainder = width - len(line)
+    spaces = round(remainder/2)
+    midtext += ' '*spaces + line + '\n'
+  return midtext
 
 def main():
   # parse arguments
   parser = argparse.ArgumentParser()
-  parser.add_argument("artist", help="The artist name.")
-  parser.add_argument("song", help="The song name.")
-  parser.add_argument("-p", "--path", nargs="?", const="None", help="Path to store lyrics.")
+  parser.add_argument("artist")
+  parser.add_argument("song")
   args = parser.parse_args()
-  artist, song = format_arguments(args.artist, args.song)
-  lyrics = get_lyrics(artist, song)
-  if lyrics:
-    lyrics = f"{artist.title()}\n\"{args.song.title()}\"\n{lyrics}"
-    if args.path:
-      with open(f"{args.path}/{artist}-{song}-lyrics.txt", "w") as file:
-      	file.write(lyrics)
-    else:
-      print(lyrics)
-    return 0
-  else:
-    return 1
+  # print lyrics
+  print(get_lyrics(args.artist, args.song))
 
 if __name__ == "__main__":
   main()
